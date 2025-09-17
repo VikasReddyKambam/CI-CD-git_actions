bbcd12a98d767b710b7afb8f49739a35f3780d0d
vikas <vikasreddykambam624@gmail.com>
2025-09-16 22:16:17 -0500

    Initial Commit

---
+++ src/main/java/com/vikas/simpleApi/controller/hello.java
@@ -0,0 +1,12 @@
+package com.vikas.simpleApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hello {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello from vikas";
+    }
+}
